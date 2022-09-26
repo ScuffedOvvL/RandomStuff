30ad49a209090bba8e49aa552b95c7cfbd3fc76e
ScuffedOvvL <114188479+ScuffedOvvL@users.noreply.github.com>
2022-09-26 18:03:16 +0200

    Update factorize.py

--- factorize.py
+++ factorize.py
@@ -1,5 +1,6 @@
 # Implementation of some factorization algorithms
-from math import gcd, isqrt ceil
+from math import gcd, sqrt, isqrt, ceil
+
 
 def pollard_rho(n):
     x, y, d = 2, 2, 1
@@ -21,6 +22,16 @@ def fermat_factorize(n):
         if p*q == n:
             return p, q
         x += 1
+    return
+
+
+def trial_division(n):
+    p = ceil(sqrt(n))
+    
+    while True:
+        if n % p == 0:
+            return n//p, p
+        p += 1
     return
 
 
